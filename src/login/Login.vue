3933efb884997197512b0e593dadfb8ca3825246
Anoob Bava <anoob.bava@gmail.com>
2019-04-02 01:14:38 +0530

    revamp the login UI

--- src/login/Login.vue
+++ src/login/Login.vue
@@ -1,44 +1,51 @@
 <template>
-  <v-container>
-    <v-layout row wrap>
-      <v-flex xs8>
-        <v-form
-          ref="form"
-          v-model="valid"
-          lazy-validation
-          xs6 >
-          <v-text-field
-            v-model="email"
-            :rules="emailRules"
-            label="E-mail"
-            required
-          ></v-text-field>
+  <v-container fluid fill-height>
+    <v-layout align-center justify-center>
+      <v-flex xs12 sm8 md4>
+        <v-card class="elevation-12">
+          <v-toolbar dark color="primary">
+            <v-toolbar-title>Login</v-toolbar-title>
+            <v-spacer></v-spacer>
+          </v-toolbar>
+          <v-card-text>
+            <v-form ref="form"
+              v-model="valid"
+              lazy-validation>
+              <v-text-field
+                prepend-icon="person"
+                v-model="email"
+                :rules="emailRules"
+                label="E-mail"
+                required>
+              </v-text-field>
 
-          <v-text-field
-            v-model="password"
-            :rules="passwordRules"
-            label="Password"
-            required
-            :append-icon="passwordShow ? 'visibility' : 'visibility_off'"
-            :type="passwordShow ? 'text' : 'password'"
-             @click:append="passwordShow = !passwordShow"
-          ></v-text-field>
-
-          <v-btn
-            :disabled="!valid"
-            color="primary"
-            @click="validate"
-          >
-            Login
-          </v-btn>
-
-          <v-btn
-            color="error"
-            @click="reset"
-          >
-            Reset Form
-          </v-btn>
-        </v-form>
+              <v-text-field
+                prepend-icon="lock"
+                v-model="password"
+                :rules="passwordRules"
+                label="Password"
+                required
+                :append-icon="passwordShow ? 'visibility' : 'visibility_off'"
+                :type="passwordShow ? 'text' : 'password'"
+                @click:append="passwordShow = !passwordShow">
+              </v-text-field>
+            </v-form>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn
+              :disabled="!valid"
+              color="primary"
+              @click="validate">
+              Login
+            </v-btn>
+            <v-spacer></v-spacer>
+            <v-btn
+              color="error"
+              @click="reset">
+              Reset Form
+            </v-btn>
+          </v-card-actions>
+        </v-card>
       </v-flex>
     </v-layout>
   </v-container>
