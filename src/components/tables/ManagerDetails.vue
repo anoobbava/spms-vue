a9e55fbedd708c552cbeb4938c0a941416363c77
Anoob Bava <anoob.bava@gmail.com>
2019-04-20 00:07:53 +0530

    added manager details component

--- src/components/tables/ManagerDetails.vue
+++ src/components/tables/ManagerDetails.vue
@@ -1,13 +1,48 @@
 <template>
-  <h3> display manager details</h3>
+  <v-data-table
+    :headers="headers"
+    :items="projectManagers"
+    hide-actions>
+
+    <template
+      slot="headerCell"
+      slot-scope="{ header }">
+      <span
+        class="font-weight-light text-warning text--darken-3"
+        v-text="header.text"
+      />
+    </template>
+
+    <template
+      slot="items"
+      slot-scope="{ index, item }">
+      <td class="text-xs-left">{{ item.name }}</td>
+      <td class="text-xs-left">{{ item.manager }}</td>
+    </template>
+  </v-data-table>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 export default {
-
+  data () {
+    return {
+      headers: [
+        {
+          text: 'Project Name',
+          sortable: true,
+          value: 'name'
+        },
+        {
+          text: 'Manager',
+          value: 'manager',
+          sortable: true
+        }
+      ]
+    }
+  },
+  computed: {
+    ...mapGetters(['projectManagers'])
+  }
 }
 </script>
-
-<style>
-
-</style>
--- src/store/modules/projects.js
+++ src/store/modules/projects.js
@@ -14,7 +14,17 @@ export default {
   getters: {
     projects: state => Object.values(state.projects),
     projectNames: state => state.projectNames,
-    project: state => projectID => state.projects.find(project => project.id === projectID)
+    project: state => projectID => state.projects.find(project => project.id === projectID),
+    projectManagers: (state) => {
+      const projects = state.projects
+      let projectManagerHash = []
+      for (let i = 0; i < projects.length; i += 1) {
+        projectManagerHash.push({ id: i,
+          name: projects[i].attributes.name,
+          manager: projects[i].attributes.manager.data.attributes.name })
+      }
+      return projectManagerHash
+    }
   },
 
   mutations: {
